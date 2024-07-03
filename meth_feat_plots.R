5d5285f40893fbdbb2ba524ce67c11096e8813ca
HarrisQ <harris.quach@gmail.com>
2024-07-03 14:42:49 -0400

    Creating repo and initial script

---
+++ meth_feat_plots.R
@@ -0,0 +1,17 @@
+#' Method by Features GGplots
+#'
+#' A visual comparison of different methods for estimating a number of features using 
+#' GGplot rather than regular R plots. This is mainly for aesthetic and presentation
+#' purposes.
+#' 
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return A number.
+
+
+
+
+
+
+
